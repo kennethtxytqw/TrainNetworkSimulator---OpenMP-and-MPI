12c070000234df023f56e4516227d8ed1b49be62
Kenneth Tan Xin You <kennethtxytqw@gmail.com>
2018-09-28 16:08:00 +0800

    Changed exec file name.

--- OmpCompileRun.sh
+++ OmpCompileRun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-g++ -o trains TrainsSimulator.cpp TrainStation.cpp TrainStationLink.cpp OmpTrainsSimulator.cpp Train.cpp OmpTrainStation.cpp OmpTrainStationLink.cpp -fopenmp
+g++ -o OmpTrainsSimulator TrainsSimulator.cpp TrainStation.cpp TrainStationLink.cpp OmpTrainsSimulator.cpp Train.cpp OmpTrainStation.cpp OmpTrainStationLink.cpp -fopenmp -std=c++11
 ./trains 1.in 2>1.error 1>1.out
 ./trains 2.in 2>2.error 1>2.out
 ./trains 3.in 2>3.error 1>3.out
